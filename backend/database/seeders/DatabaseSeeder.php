5657f5c413ffc09d21437de95a11252588339a22
Cynthia Rubio <cynrusan@gmail.com>
2026-04-14 19:14:10 +0200

    feat(templates): seeders and mocks for template_blocks and template_versions

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -21,6 +21,8 @@ class DatabaseSeeder extends Seeder
             GroupsSeeder::class,
             TemplatesSeeder::class,
             TemplateReviewersSeeder::class,
+            TemplateBlocksSeeder::class,
+            TemplateVersionsSeeder::class,
         ]);
     }
 }
