11434bd6d470c917409478a94586f8e434496eaf
Cynthia Rubio <cynrusan@gmail.com>
2026-04-14 18:58:01 +0200

    feat: split base migrations for templates and reviewers; add mock seeders

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -19,6 +19,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersSourceSeeder::class,
             GroupsSeeder::class,
+            TemplatesSeeder::class,
+            TemplateReviewersSeeder::class,
         ]);
     }
 }
